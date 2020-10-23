82430d011aecf9fff40cdbc7a22688569190fce8
suzichen <suzichenapk@outlook.com>
2020-10-23 16:01:49 +0800

    update build version

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -11,24 +11,24 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  '/precache-manifest.5adb50eeb95e808b65cdd43947de31fb.js'
-)
+  "/precache-manifest.3c30cf75edbdeba999e05b1e22115b75.js"
+);
 
-workbox.core.setCacheNameDetails({ prefix: 'guide-h5' })
+workbox.core.setCacheNameDetails({prefix: "guide-h5"});
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting()
+    self.skipWaiting();
   }
-})
+});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
-self.__precacheManifest = [].concat(self.__precacheManifest || [])
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
