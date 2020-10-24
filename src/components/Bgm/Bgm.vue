5f40d39173a5dda9166b6f2a50a7f429559c1d3f
Suzic <suzichenapk@gmail.com>
2020-10-24 17:53:30 +0800

    update: 代码优化

--- src/components/Bgm/Bgm.vue
+++ src/components/Bgm/Bgm.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <audio id="audioEl" ref="audioRef" :src="bgmSrc" autoplay loop hidden></audio>
+    <audio ref="audioRef" :src="bgmSrc" :autoplay="!disabled" loop hidden></audio>
     <img :src="disabled ? muteImg : musicImg" :style="styles" @click="toggle">
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { computed, defineComponent, onMounted, ref } from 'vue'
 import musicImg from '@/assets/imgs/music.svg'
 import muteImg from '@/assets/imgs/mute.svg'
 import bgmSrc from '@/assets/bgm.mp3'
@@ -16,23 +16,32 @@ export default defineComponent({
   props: {
     size: {
       type: Number
+    },
+    modelValue: {
+      type: Boolean,
+      default: false
     }
   },
-  setup (props) {
-    const disabled = ref(false)
+  setup (props, context) {
+    const { emit } = context
     const audioRef = ref({} as HTMLAudioElement)
-    const toggle = () => {
-      disabled.value = !disabled.value
-      if (audioRef.value.paused) {
-        audioRef.value.play()
+    const disabled = computed(() => props.modelValue)
+    const togglePlay = (play: boolean) => {
+      if (play) {
+        audioRef.value.paused && audioRef.value.play()
       } else {
-        audioRef.value.pause()
+        !audioRef.value.paused && audioRef.value.pause()
       }
+    }
+    const toggle = () => {
+      emit('update:modelValue', !props.modelValue)
+      togglePlay(disabled.value)
     }
     const styles = {
       width: props.size + 'px',
       height: props.size + 'px'
     }
+    onMounted(() => { togglePlay(!disabled.value) })
     return {
       audioRef,
       toggle,
